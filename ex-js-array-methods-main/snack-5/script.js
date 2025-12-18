41922b701c6e9e3edeffce05c79a224afa56de46
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-18 15:54:12 +0100

    Sncak 5 vari modi

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -6,3 +6,27 @@ const nums = [2, 8, 4, 7, 12, 87];
 const numsEven = nums.filter((num) => num % 2 === 0 ? true : false); 
 
 console.log(numsEven);
+
+//con for each
+
+const numsEven2 = [];
+
+nums.forEach((num, i) =>{
+    if (num % 2 === 0){
+        numsEven2.push(num)
+    }
+});
+
+console.log(numsEven2);
+
+//con for
+
+const numsEven3 = [];
+
+for (i = 0; i < nums.length; i++){
+     if (nums[i] % 2 === 0){
+        numsEven3.push(nums[i])
+    }
+}
+
+console.log(numsEven3);
