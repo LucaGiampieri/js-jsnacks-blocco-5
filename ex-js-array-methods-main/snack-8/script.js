674f0c89f55ec9285eb1ca1d052d3f230304c63b
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-18 15:28:33 +0100

    Snack 8

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,38 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+//metodo 1
+
+let marcoLanciClass;
+
+const marcoLanci = students.find((student) =>{
+  if(student.name === 'Marco Lanci'){
+    marcoLanciClass = student.class 
+  }
+  return marcoLanciClass 
+});
+
+console.log(marcoLanciClass);
+
+//meteodo 2
+
+const marcoLanciClass2 = students.find((student) =>{
+  if(student.name === 'Marco Lanci'){
+    return student
+  }
+});
+
+console.log(marcoLanciClass2.class);
+
+//metodo 3
+
+const marcoLanci3 = students.find((student) =>{
+  if(student.name === 'Marco Lanci'){
+    return student
+  }
+});
+ 
+const marcoLanciClass3 = marcoLanci3.class;
+
+console.log(marcoLanciClass3);
