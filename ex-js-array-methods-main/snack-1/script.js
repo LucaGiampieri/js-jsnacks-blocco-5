be6e716ba266f7901bcb3ed1e6f861201f1f632b
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-18 19:00:54 +0100

    Snack 1 fix

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -3,24 +3,17 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
-console.log(names);
 
 //con for each
 
-let newList =[];
-
 names.forEach((name,i) => {
-    newList.push(name);
+    console.log(name);
 });
 
-console.log(newList);
 
 //con for
 
-let newList2 =[];
-
 for (let i = 0; i < names.length; i++){
-    newList2.push(names[i])
+    console.log(names[i]);
 };
 
-console.log(newList2);
